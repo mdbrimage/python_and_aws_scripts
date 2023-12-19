f2d30367743f3809e3eea5435883109d79d3060e
Michael Brimage <mbrimage58@gmail.com>
2023-12-19 20:39:48 +0000

    uploads an new object to an existing bucket in AWS s3

--- upload_new_object.py
+++ upload_new_object.py
@@ -2,7 +2,7 @@ import boto3
 
 s3 = boto3.client('s3')
 
-#The code below uploads an existing object to an existing bucket in AWS s3
+#The code below uploads an new object to an existing bucket in AWS s3
 
 s3.put_object(Bucket ='mbrimage-boto3-12122023', Key = 'test_text_string.txt', Body ="Hey this is a string"
 , ContentType = 'text/plain')
