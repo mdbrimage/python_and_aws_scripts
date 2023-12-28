7a43b04608b66b8f7c344683a3e3d1206cd6431a
Michael Brimage <mbrimage58@gmail.com>
2023-12-28 22:48:24 +0000

    Python Functions to retrieve AWS VPC information

--- vpc/list_vpcs.py
+++ vpc/list_vpcs.py
@@ -1,7 +1,29 @@
 import boto3
 
+
+def get_vpc_information(client, filters = []):
+    response = ec2.describe_vpcs(Filters = filters)
+    for vpc in response['Vpcs']:
+        print(vpc['VpcId'], vpc['CidrBlock'], vpc["IsDefault"])
+    
+
+def get_vpc_name(client, filter = []):
+    response = ec2.describe_vpcs(Filter = filters)
+    for vpc in response['Vpcs']:
+        if 'Tags' in vpc:
+            for tag in vpc['Tags']:
+                if 'Name' == tag['Key']:
+                    print(tag['Value'])
+             
+
+
+
+if __name__ ==  '__main__':
+
 ec2 = boto3.client('ec2')
 
-response = ec2.describe_vpcs()
+Filters = [{'Name': 'isDefault', 'Values': ['true',]},]
 
-print(response)
+
+get_vpc_information(ec2)
+get_vpc_information(ec2, Filters)
